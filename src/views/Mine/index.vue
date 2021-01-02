64281389995d19700b5ac5552c2d8c3a831817c9
mkyn07 <yk454862540@qq.com>
2021-01-02 16:45:01 +1100

    add components

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <Header title="🚀我的湖人🚀"/>
+    <div id="content">
+      <Login/>
+
+    </div>
+    <TabBar/>
+  </div>
+
+</template>
+
+<script>
+
+import Header from '@/components/Header';
+import TabBar from '@/components/TabBar';
+import Login from '@/components/Login';
+export default {
+  name: "Mine",
+  components:{
+          Login,
+          Header,
+          TabBar,
+
+        }
+}
+</script>
+
+<style scoped>
+
+</style>
